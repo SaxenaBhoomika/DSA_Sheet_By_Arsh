95292d925c43e71569d50cbf42462acfca765f31
SaxenaBhoomika <89317642+SaxenaBhoomika@users.noreply.github.com>
2023-07-04 13:38:40 +0530

    Create longestCommonPrefix.cpp

---
+++ Day2/longestCommonPrefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res="";
+        int n=strs.size();
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string last=strs[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return res;
+            }
+            res+=first[i];
+        }
+        return res;
+    }
+};
