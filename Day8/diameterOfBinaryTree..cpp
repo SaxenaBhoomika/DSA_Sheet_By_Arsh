a1b8addaf8f0df6360112f37b96d6cd769d11c43
SaxenaBhoomika <89317642+SaxenaBhoomika@users.noreply.github.com>
2023-07-15 12:47:55 +0530

    Create diameterOfBinaryTree..cpp

---
+++ Day8/diameterOfBinaryTree..cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int height(TreeNode* root, int &maxi){
+        if(root==NULL){
+            return 0;
+        }
+        int lh=height(root->left,maxi);
+        int rh=height(root->right,maxi);
+        maxi=max(maxi,lh+rh);
+        return 1+max(lh,rh);
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi=0;
+        height(root,maxi);
+        return maxi;
+        
+    }
+};
